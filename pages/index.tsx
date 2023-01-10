41eaaa3f46652477a388b33c62b7b6b8d9b71267
Paolo Conizzoli <paolo.conizzoli@gmail.com>
2023-01-10 20:07:01 +0100

    Slightly improved page text content

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,22 +59,22 @@ export default function Home({year}: InferGetStaticPropsType<typeof getStaticPro
                     <div className="mb-12">
                         <Image className="rounded-full" src="/me.jpg" width="150px" height="150px" alt={"Paolo portrait"}></Image>
                     </div>
-                    <h1>{`Hey 🤙 I'm Paolo`}</h1>
+                    <h1>{`Hey! I'm Paolo`}</h1>
                     <p className={"text-xl mt-4 text-slate-400"}>
                         {`I'm a Software Developer and Project Manager, living in 🇮🇹.`}<br/>
                     </p>
 
                     <section className={"md:w-9/12 lg:w-7/12 mx-auto"}>
-                        <p className={"mt-12 mb-4"}>I have <strong className={"text-blue-500"}>over seven years of
+                        <p className={"mt-12 mb-4"}>I have <strong className={"text-blue-500"}>eight years of
                             experience in software development</strong>, working on different
-                            projects for size and technology used. Over the last few years, in several cases, as coordinator
+                            projects for size and technology used. Over the years, in several cases, as coordinator
                             and project manager.</p>
                         <p className={"mt-2 mb-4"}>I consider myself <strong className={"text-blue-500"}>very curious,
                             fast learner and proficient handling my time</strong>; I also have strong teamwork and
                             leadership skills. Because of these qualities, in the last several years, I have been
                             entrusted with many responsibilities. These duties also allowed me to improve in many areas
                             of interest.</p>
-                        <p className={"mt-2 mb-4"}>During my relatively short career, I had the opportunity to fill
+                        <p className={"mt-2 mb-4"}>During my career, I had the opportunity to fill
                             different roles and perform numerous activities types. This dynamism allows me to
                             <strong className={"text-blue-500"}> master the handling of the entire production workflow
                             </strong> and integrate different operational groups.</p>
